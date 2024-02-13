c19b715864f61bd566d44612d4b51a731c5f2ad1
lucasfregonezi <lucasfregonezi@gmail.com>
2024-02-13 11:49:46 -0300

    Aula3

---
+++ app/routes/Routes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\routes;
+
+class Routes
+{
+    public static function get()
+    {
+        return [
+            'get'  => [
+                '/'            => 'HomeController@index',
+                '/user/[0-9]+' => 'UserController@show',
+                '/register'    => 'RegisterController@store'
+            ],
+            'post' => []
+        ];
+    }
+}
