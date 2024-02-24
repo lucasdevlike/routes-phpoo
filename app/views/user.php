3c68eeee406e493b67cb5b8eb1146ca57b80c787
lucasfregonezi <lucasfregonezi@gmail.com>
2024-02-24 18:31:46 -0300

    Aula28 - Como criar flash messages com php

--- app/views/user.php
+++ app/views/user.php
@@ -2,12 +2,18 @@
 
 <h1>User</h1>
 
+<?php echo flash('created');?>
 <form action="/user/update" method="post">
+    <?php echo flash('firstName');?>
     <input type="text" name="firstName" value="Lucas">
+    <?php echo flash('lastName');?>
     <input type="text" name="lastName" value="Moraes">
     <?php echo getToken(); ?>
+    <?php echo flash('email');?>
     <input type="mail" name="email" value="lucasmoraes@email.com">
+    <?php echo flash('password');?>
     <input type="password" name="password" value="123456">
+    <?php echo flash('');?>
 
     <button type="submit">Atualizar</button>
 </form>
