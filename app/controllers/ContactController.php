bbedf6e0b49c5090e07dff558451764fb196d9f9
lucasfregonezi <lucasfregonezi@gmail.com>
2024-03-10 11:27:30 -0300

    Aula 33 - Como enviar emails | parte 2

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -3,6 +3,8 @@
 namespace app\controllers;
 
 use app\support\Email;
+use app\support\Flash;
+use app\support\Validate;
 
 class ContactController extends Controller
 {
@@ -13,8 +15,34 @@ class ContactController extends Controller
 
     public function store()
     {
+        $validate = new Validate();
+        $validated = $validate->validate([
+            'email'   => 'email|required',
+            'subject' => 'required',
+            'message' => 'required'
+        ]);
+
+        if (!$validated) {
+            return redirect('/contact');
+        }
+
         $email = new Email();
-        $email->from()->to([''])->template('contact')->message()->subject()->send();
+        // $email->from()->to([''])->template('contact')->message()->subject()->send();
+        $sent = $email->from($validated['email'], 'Lucas Moraes')
+                      ->to('lucasfregonezi@gmail.com')
+                      ->message($validated['message'])
+                      ->subject($validated['subject'])
+                      ->send();
+
+        if ($sent) {
+            Flash::set('sent_success', 'Email enviado com sucesso');
+            return redirect('/contact');
+        } else {
+            Flash::set('sent_error', 'Ocorreu um erro ao enviar o email');
+            return redirect('/contact');
+
+        }
+        // dd($sent);
     }
 
 }
--- app/views/contact.php
+++ app/views/contact.php
@@ -1,12 +1,17 @@
 <?php $this->layout('master', ['title' => $title]) ?>
 
 <h1>Contato</h1>
-
+<?php echo flash('sent_success', 'color:green;'); ?>
+<?php echo flash('sent_error', 'color:red;'); ?>
 <form action="/contact" method="post">
-
+    <?php echo flash('email'); ?>
     <input type="text" name="email" id="" placeholder="email"><br>
+    <?php echo flash('subject'); ?>
     <input type="text" name="subject" id="" placeholder="assunto"><br>
+    <?php echo flash('message'); ?>
     <textarea name="message" id="" cols="30" rows="10" placeholder="Mensagem"></textarea><br>
+
+    <?php echo getToken(); ?>
 
     <button type="submit">Send Mail</button>
 
